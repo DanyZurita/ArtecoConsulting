71d081806ef88cadaab12e3def1f8a1da1fa6e7c
DanyZurita <dzurita@cifpfbmoll.eu>
2020-08-07 04:55:52 +0200

    feat(main): Añadir el mock a una variable string

--- src/main/java/edu/elsmancs/arteco/main.java
+++ src/main/java/edu/elsmancs/arteco/main.java
@@ -1,10 +1,9 @@
 package edu.elsmancs.arteco;
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String loren = Crawler.mockURL();
+		
 	}
-
 }
