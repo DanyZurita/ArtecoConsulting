a693c1b166f28f70144f152955cb23ae0ecac466
DanyZurita <dzurita@cifpfbmoll.eu>
2020-08-07 22:27:55 +0200

    feat(Logica): Añadir lógica de totalPhrases() usando BreakIterator

--- src/main/java/edu/elsmancs/arteco/Logica.java
+++ src/main/java/edu/elsmancs/arteco/Logica.java
@@ -1,5 +1,6 @@
 package edu.elsmancs.arteco;
 
+import java.text.BreakIterator;
 import java.util.StringTokenizer;
 
 public class Logica {
@@ -7,8 +8,32 @@ public class Logica {
 	public static Integer totalWords(String loren) {
 		StringTokenizer st = new StringTokenizer(loren);
 		Integer numWords = st.countTokens();
-		System.out.println ("palabras: " + numWords);
+		System.out.println ("Palabras: " + numWords);
 		return numWords;
+	}
+	
+	public static Integer totalPhrases(String loren) {
+		BreakIterator bi = BreakIterator.getSentenceInstance();
+		bi.setText(loren);
+		Integer numPhrases = 0;
+		int index = bi.first();
+		String sentence = "";
+		
+		for (int end = bi.next();
+		     end != BreakIterator.DONE;
+		     index = end, end = bi.next()) {
+			sentence = loren.substring(index,end);
+		    //System.out.println("Sentence: " + sentence);
+		    
+		    //Comprueba que la frase acabe en "." para el contador
+		    if (sentence.lastIndexOf(".") != -1) {
+		    	numPhrases += 1;
+		    }
+		    
+		}
+		
+		System.out.println ("Frases: " + numPhrases);
+		return numPhrases;
 	}
 	
 }
