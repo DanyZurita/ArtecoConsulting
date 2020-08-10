f2e64d73f6dbf1a2eaad917e650649861b51862c
DanyZurita <dzurita@cifpfbmoll.eu>
2020-08-10 18:57:18 +0200

    feat(Logica): Añadir topWords() usando Hashmap, List<>, y funciones lambda

--- src/main/java/edu/elsmancs/arteco/Logica.java
+++ src/main/java/edu/elsmancs/arteco/Logica.java
@@ -1,7 +1,13 @@
 package edu.elsmancs.arteco;
 
 import java.text.BreakIterator;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 public class Logica {
 	
@@ -15,17 +21,20 @@ public class Logica {
 	
 	public static Integer totalPhrases(String loren) {
 		String lorenClean = removePHtmlNotations(loren);
+		System.out.print(lorenClean);
 		BreakIterator bi = BreakIterator.getSentenceInstance();
 		bi.setText(lorenClean);
 		Integer numPhrases = 0;
 		int index = bi.first();
 		String sentence = "";
+		int i = 0;
 		
 		for (int end = bi.next();
 		     end != BreakIterator.DONE;
 		     index = end, end = bi.next()) {
 			sentence = lorenClean.substring(index,end);
-		    //System.out.println("Sentence: " + sentence);
+			i++;
+		    System.out.println("\nSentence "+ i + ": " + sentence);
 		    
 		    //Comprueba que la frase acabe en "." para el contador
 		    if (sentence.lastIndexOf(".") != -1) {
@@ -86,4 +95,41 @@ public class Logica {
 		return numPalindroms;
 	}
 	
+	public static Map<String, Integer> topWords(String loren, int topMax) {
+		final int TOP = topMax;
+		Map<String, Integer> words = new HashMap<String, Integer>();
+		String lorenClean = removePHtmlNotations(loren.replaceAll("\\.", "")).toLowerCase();
+		StringTokenizer st = new StringTokenizer(lorenClean);
+		//Crear HashMap con el acumulador por repetición
+		while (st.hasMoreTokens()) {
+			String token = st.nextToken();
+			words.compute(token, (word, count) -> {if (count == null)
+                return 1;
+            return count + 1;});
+		}
+		
+		//Lista para saber las palabras que más se repiten del HashMap
+		List<Integer> topCounts = words.values().stream()
+							                .sorted(Comparator.reverseOrder())
+							                .limit(TOP)
+							                .collect(Collectors.toList());
+		
+		//Guardar estado original del contador anterior
+		final List<Integer> topCounts2 = words.values().stream()
+                .sorted(Comparator.reverseOrder())
+                .limit(TOP)
+                .collect(Collectors.toList());
+		
+        //Lista de las palabras más repetidas
+		final String[] topWords = new String[TOP];
+        words.forEach((word, count) -> {
+            int indexOfWord = topCounts.indexOf(count);
+            if (indexOfWord > -1) {
+                topWords[indexOfWord] = word;
+                topCounts.set(indexOfWord, -1);
+            }
+        });
+        System.out.println("\nTop 5 palabras más repetidas: " + Arrays.toString(topWords) + "\nRespectivas cuentas: " + topCounts2);
+		return words;
+	}
 }
