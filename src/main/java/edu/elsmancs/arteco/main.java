c39146570f59f55a7f72040dee2ee1156a621529
DanyZurita <dzurita@cifpfbmoll.eu>
2020-08-10 18:59:26 +0200

    refactor(Main): Cambiar el crawker por getURLJSoup() y añadir tioWords()

--- src/main/java/edu/elsmancs/arteco/main.java
+++ src/main/java/edu/elsmancs/arteco/main.java
@@ -1,12 +1,15 @@
 package edu.elsmancs.arteco;
 
+
 public class Main {
 
 	public static void main(String[] args) {
-		String loren = Crawler.mockURL();
+		String loren = Crawler.getURLJSoup("https://es.lipsum.com/feed/html");
+		//String loren = Crawler.mockURL();
 		Logica.totalWords(loren);
 		Logica.totalPhrases(loren);
 		Logica.totalParagraphs(loren);
 		Logica.totalPalindroms(loren);
+		Logica.topWords(loren, 5);
 	}
 }
