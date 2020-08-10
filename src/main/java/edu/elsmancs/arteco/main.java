e081dba9720ddb7e05df9840c2c560e7907e205d
DanyZurita <dzurita@cifpfbmoll.eu>
2020-08-10 20:31:40 +0200

    refactor(Main): Añadir topTuples() y formatear cada apartado con prints()

--- src/main/java/edu/elsmancs/arteco/main.java
+++ src/main/java/edu/elsmancs/arteco/main.java
@@ -4,12 +4,20 @@ package edu.elsmancs.arteco;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("\n - OBTENER LOS 5 PRIMEROS PARRAFOS - \n");
 		String loren = Crawler.getURLJSoup("https://es.lipsum.com/feed/html");
 		//String loren = Crawler.mockURL();
+		System.out.println("\n\n - OBTENER EL TOTAL DE PALABRAS - \n");
 		Logica.totalWords(loren);
+		System.out.println("\n\n - OBTENER EL TOTAL DE FRASES - \n");
 		Logica.totalPhrases(loren);
+		System.out.println("\n\n - OBTENER EL TOTAL DE PARRAFOS - \n");
 		Logica.totalParagraphs(loren);
+		System.out.println("\n\n - IDENTIFICAR Y CONTAR PALÍNDROMOS - \n");
 		Logica.totalPalindroms(loren);
+		System.out.println("\n\n - IDENTIFICAR Y CONTAR TOP 5 PALABRAS REPETIDAS - \n");
 		Logica.topWords(loren, 5);
+		System.out.println("\n\n - IDENTIFICAR Y CONTAR TOP 5 TUPLAS REPETIDAS - \n");
+		Logica.topTuples(loren, 5);
 	}
 }
